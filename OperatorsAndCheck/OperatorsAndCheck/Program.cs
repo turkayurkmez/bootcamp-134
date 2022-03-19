812081ed68906feefe72d8f45ae86a6811953162
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-03-19 13:25:25 +0300

    try-catch and check explained

---
+++ OperatorsAndCheck/OperatorsAndCheck/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace OperatorsAndCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = ((3 * 5)+9)/2;
+            string newWord = "Bootcamp" + 134;
+            Console.WriteLine(newWord);
+            Console.WriteLine(x.ToString());
+
+            byte bigNumber = 254;
+            byte smallNumber = 3;
+
+
+            try
+            {
+                checked
+                {
+                    byte total = (byte)(bigNumber + smallNumber);
+                    Console.WriteLine(total);
+                }
+            }
+            catch (System.OverflowException ex)
+            {
+
+                Console.WriteLine("Total value cannot cast to byte data type");
+            }
+
+            Console.WriteLine("Number 1 is:");
+           
+
+            try
+            {
+                int number1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Number 2 is:");
+                int number2 = Convert.ToInt32(Console.ReadLine());
+                int division = number1 / number2;
+                Console.WriteLine($"Result {division} " );
+            }
+            catch (FormatException)
+            {
+
+                Console.WriteLine("Please just give digits");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Number 2 cannot be 0");
+            }
+
+
+
+        }
+    }
+}
