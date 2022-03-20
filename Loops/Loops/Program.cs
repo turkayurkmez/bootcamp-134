f24b609fe21a5e8600d1c6a7c71d69a0ccd9d209
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-03-20 13:04:35 +0300

    introduction to loops

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             *  Sayı tahmin oyunu:
+             *  1. Program rastgele bir sayı üretir.
+             *  2. Kullanıcıdan bir tahmin istenir.
+             *  3. Girilen tahmine göre aşağı ya da yukarı biçiminde yönlendirilir.
+             *  4. Sayı bilinene dek 2. adıma dönülür.
+             */
+            bool isGameFinished = false;
+            Random randomNumberGenerator = new Random();
+            //  1. Program rastgele bir sayı üretir.
+            int randomNumber = randomNumberGenerator.Next(0, 100);
+            while (!isGameFinished)
+            {
+                // 2. Kullanıcıdan bir tahmin istenir.
+                Console.WriteLine("Tahmininizi girin:");
+                int guess = Convert.ToInt32(Console.ReadLine());
+                // 3. Girilen tahmine göre aşağı ya da yukarı biçiminde yönlendirilir.
+                if (guess < randomNumber)
+                {
+                    Console.WriteLine("Yukarı!");
+                }
+                else if (guess > randomNumber)
+                {
+                    Console.WriteLine("Aşağı!");
+                }
+                else
+                {
+                    Console.WriteLine("Bildiniz!");
+                    isGameFinished = true;
+                }
+            }
+
+        }
+    }
+}
