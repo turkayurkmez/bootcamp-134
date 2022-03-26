b85280c1d5d75750cf255380b8b68b39063ea683
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-03-26 12:36:14 +0300

    samples for built-in methods

--- BuiltInMethods/BuiltInMethods/Program.cs
+++ BuiltInMethods/BuiltInMethods/Program.cs
@@ -25,17 +25,63 @@ namespace BuiltInMethods
                 startIndex = findingIndex + 1;
 
             }
+
             //Console.WriteLine(index);
 
             //Başka bir örnek....
             List<string> emails = new List<string>()
             {
                 "turkay.urkmez@dinamikzihin.com",
-                "kirikkalp72@gmail.com",
+                "kirikkalp72@yahoo.com",
                 "nur.bilge@microsoft.com",
                 "turkay.urkmez@gmail.com",
-                "babyboy@mynet.com"
+                "babyboy@mynet.com",
+                "testmail"
+               
             };
+
+            List<string> publicDomains = new List<string>()
+            {
+                "yahoo.com",
+                "gmail.com",
+                "mynet.com"
+            };
+
+            List<string> companyEmails = new List<string>();
+
+            foreach (var mail in emails)
+            {
+                //foreach (var domain in publicDomains)
+                //{
+                //    if (!mail.EndsWith(domain))
+                //    {
+
+                //    }
+                //}
+                string[] mailParts = mail.Split('@');
+               
+                if (mailParts.Length>1)
+                {
+                    string mailDomain = mailParts[1];
+                    bool isExists = publicDomains.Contains(mailDomain);
+                    if (!isExists)
+                    {
+                        companyEmails.Add(mail);
+                    }
+                }
+              
+               
+            }
+            Console.WriteLine("Şirket e-posta adresleri:");
+            foreach (var mailAdrees in companyEmails)
+            {
+                Console.WriteLine(mailAdrees);
+            }
+
+            List<string> cities = new List<string>() { "Ankara", "Ankara", "Ankara", "Sinop", "İstanbul", "İstanbul", "Eskişehir", "Ankara" };
+
+
+
 
 
         }
