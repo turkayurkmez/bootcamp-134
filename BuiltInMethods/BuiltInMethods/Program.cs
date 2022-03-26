ab66cfa7e514972614d6de9510ab474b1ae707c4
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-03-26 11:28:48 +0300

    Built-in methods explained

---
+++ BuiltInMethods/BuiltInMethods/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BuiltInMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir kelime giriniz");
+
+            string word = Console.ReadLine();
+
+            //int index = word.IndexOf('i', 5);
+            int startIndex = 0;
+            Console.WriteLine("Aradığınız harf:");
+            string charackter = Console.ReadLine();
+
+            Console.Write($"{charackter} harfinin index değerleri:");
+
+            while (word.IndexOf(charackter, startIndex) != -1)
+            {
+                int findingIndex = word.IndexOf(charackter, startIndex);
+                Console.Write($"{findingIndex}, ");
+                startIndex = findingIndex + 1;
+
+            }
+            //Console.WriteLine(index);
+
+            //Başka bir örnek....
+            List<string> emails = new List<string>()
+            {
+                "turkay.urkmez@dinamikzihin.com",
+                "kirikkalp72@gmail.com",
+                "nur.bilge@microsoft.com",
+                "turkay.urkmez@gmail.com",
+                "babyboy@mynet.com"
+            };
+
+
+        }
+    }
+}
---
+++ LoopInsideLoop/LoopInsideLoop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LoopInsideLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * çarpım tablosu...
+             * 2 x 1 =2
+             * 2 x 2 =4
+             * 2 x 9 = 18
+             * 
+             * 3 x 1 = 3
+             */
+
+            for (int i = 2; i <= 9; i++)
+            {
+                for (int y = 1; y <= 9; y++)
+                {
+                    Console.WriteLine($"{i} x {y} = {i * y}");
+
+                }
+                Console.WriteLine("-----------------------------");
+            }
+        }
+    }
+}
