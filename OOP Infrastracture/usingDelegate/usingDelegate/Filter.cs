590218d52177875e1fc136e329601369e7f0b49f
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-04-10 14:48:51 +0300

    Delegation and lamda operators

---
+++ OOP Infrastracture/usingDelegate/usingDelegate/Filter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace usingDelegate
+{
+   public static class Filters
+    {
+        //.NET 4.0
+        //public delegate bool Criteria(int item);
+
+
+       public static List<int> Filter(List<int> values, Func<int, bool> criteria )
+        {
+            List<int> filtered = new List<int>();
+            foreach (var item in values)
+            {
+                if (criteria(item))
+                {
+                    filtered.Add(item);
+                }
+            }
+
+            return filtered;
+        }
+
+    }
+}
---
+++ OOP Infrastracture/usingDelegate/usingDelegate/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace usingDelegate
+{
+    class Program
+    {
+        static List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        static void Main(string[] args)
+        {
+            /*
+             * yukarıdaki numbers isimli array içindeki sayıları programcının dilediği gibi filtrelemesini istiyoruz
+             */
+            //.NET 1.0
+            //var filtered = Filters.Filter(numbers, isMultiply3);
+
+            //.NET 2.0:
+            var filtered = Filters.Filter(numbers,delegate (int c)  {  return c < 5;    });
+            //.NET 3.5:
+            var filtered2 = Filters.Filter(numbers, c => c % 5 == 0);
+
+            //foreach (var item in filtered2)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            filtered2.ForEach(p => Console.WriteLine(p));
+
+            filtered2.Where(x => x % 2 == 0);
+        }
+
+        static bool isEven(int item)
+        {
+            return item % 2 == 0;
+        }
+
+        static bool isMultiply3(int item)
+        {
+            return item % 3 == 0;
+        }
+    }
+}
